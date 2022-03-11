25669b39f3a36f12517f6681850ee087645981e2
EugeneSince1993 <jk235@mail.ru>
2022-03-11 14:34:27 +0300

    lesson 97 paused at 28-30

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 import userPhoto from "../../../assets/images/user.png";
+import ProfileDataForm from "./ProfileDataForm";
 
 const ProfileInfo = ({profile, status, updateStatus,
                        currentUserId, authorizedUserId, savePhoto}) => {
+
+  let isOwner = currentUserId == authorizedUserId;
+
+  let [editMode, setEditMode] = useState(false);
 
   if (!profile) {
     return <Preloader />;
@@ -24,33 +29,59 @@ const ProfileInfo = ({profile, status, updateStatus,
           src={profile.photos.large || userPhoto}
           className={s.mainPhoto}
         />
-        { currentUserId == authorizedUserId &&
-            <input type={"file"} onChange={onMainPhotoSelected} /> }
-
-        <div>
-          <div>
-            <b>Full name</b>: {profile.fullName}
-          </div>
-          <div>
-            <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
-          </div>
-          {
-            profile.lookingForAJob &&
-              <div>
-                <b>My professional skills</b>: {profile.lookingForAJobDescription}
-              </div>
-          }
-          <div>
-            <b>About me</b>: {profile.aboutMe}
-          </div>
-          <div>
-            <b>Contacts</b>: {profile.contacts}
-          {/* 7.50 "Error: Objects are not valid as a React child..." */}
-          </div>
-        </div>
-
+        { isOwner && <input type={"file"} onChange={onMainPhotoSelected} /> }
+        { editMode
+          ? <ProfileDataForm profile={profile} />
+          : <ProfileData goToEditMode={ () => { setEditMode(true); } } profile={profile} isOwner={isOwner} /> }
         <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
       </div>
+    </div>
+  );
+};
+
+const ProfileData = ({profile, isOwner, goToEditMode}) => {
+  return (
+    <div>
+      {
+        isOwner &&
+          <div>
+            <button onClick={goToEditMode}>edit</button>
+          </div>
+      }
+      <div>
+        <b>Full name</b>: {profile.fullName}
+      </div>
+      <div>
+        <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
+      </div>
+      {
+        profile.lookingForAJob &&
+        <div>
+          <b>My professional skills</b>: {profile.lookingForAJobDescription}
+        </div>
+      }
+      <div>
+        <b>About me</b>: {profile.aboutMe}
+      </div>
+      <div>
+        <b>Contacts</b>:
+        {
+          Object.keys(profile.contacts)
+            .map(key => {
+              return (
+                <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+              );
+            })
+        }
+      </div>
+    </div>
+  );
+};
+
+const Contact = ({contactTitle, contactValue}) => {
+  return (
+    <div className={s.contact}>
+      <b>{contactTitle}</b>: {contactValue}
     </div>
   );
 };
